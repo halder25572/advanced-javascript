af9e9dbe8959d9e6ab51c29b041ce4513faebaa5
Nayan Halder <halder25572@gmail.com>
2021-02-02 12:23:54 +0600

    numm vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"Smart Dud", phone:45820,};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 3, 5]
+console.log(ages[11]);
